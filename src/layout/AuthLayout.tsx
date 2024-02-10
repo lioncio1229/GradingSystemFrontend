074b4c633cc2414d7477d52d71cd4f7cc81b3045
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2024-02-10 11:53:24 +0800

    Update logo

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -9,9 +9,9 @@ export default function AuthLayout() {
                 bgcolor: "secondary.main",
                 alignItems: "center",
                 p: 2
-            }}>
-                <Stack flexDirection="row" alignItems="center">
-                    <Box component="img" src={Logo} sx={{width: 100, height: "auto"}} />
+            }} gap={1}>
+                <Stack flexDirection="row" alignItems="center" gap={1}>
+                    <Box component="img" src={Logo} sx={{width: 120, height: "auto"}} />
                     <Typography variant="h1" fontSize={42} color="primary">PHILTECH</Typography>
                 </Stack>
                 <Typography sx={{color: "primary.dark"}}>Balibago Commercial Complex, GS-B, Brgy Balibago, Sta Rosa, Laguna</Typography>
